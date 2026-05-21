b37d981466f96921cffd9ae43e8dbc98f369d95e
Kobe Li <likobe23@gmail.com>
2026-05-21 12:57:05 -0700

    Fix middleware

--- middleware/logger.go
+++ middleware/logger.go
@@ -7,7 +7,7 @@ import (
 
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("%s: %s%s%s\n", r.Method, r.Host, r.URL.Path, r.URL.RawQuery)
+		fmt.Printf("%s: %s%s?%s\n", r.Method, r.Host, r.URL.Path, r.URL.RawQuery)
 		next.ServeHTTP(w, r)
 	})
 }
